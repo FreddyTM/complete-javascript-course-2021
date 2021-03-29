76c7428b6a4587b349f26a8b02f702a3f76be1c6
Freddy <atm64ioc@gmail.com>
2021-03-29 13:34:37 +0200

    btnHold eventListener in progress...

--- complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -6,6 +6,7 @@ const score0El = document.querySelector('#score--0');
 //Getting element by id with getElementById (no neeo of #)
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -21,6 +22,18 @@ diceEl.classList.add('hidden');
 
 let currentScore = 0;
 let activePlayer = 0;
+const switchPlayer = function () {
+  //Set current score back to 0
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  //Switch player
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  //Change background
+  /*toggle() removes a class if the element has it, or adds it if the element
+  doesn't have it*/
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
 
 //Button listeners
 btnRoll.addEventListener('click', function () {
@@ -28,7 +41,7 @@ btnRoll.addEventListener('click', function () {
   const diceNumber = Math.trunc(Math.random() * 6) + 1;
   //2. Display dice
   diceEl.classList.remove('hidden');
-  /*Instead of an if or switch statement, we'll use a template litera
+  /*Instead of an if or switch statement, we'll use a template literal
   to directly assign the name of the image source*/
   diceEl.src = `dice-${diceNumber}.png`;
   //3. Check for rolled 1. If true, switch to next player
@@ -40,14 +53,22 @@ btnRoll.addEventListener('click', function () {
       `current--${activePlayer}`
     ).textContent = currentScore;
   } else {
-    //Set current score back to 0
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    //Switch player
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    /*toggle() removes a class if the element has it, or adds if the element
-    doesn't have it*/
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
-    //Change background
+    switchPlayer();
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  //Add current score to active player score
+  /* Number(`score${activePlayer}El`.textContent) += currentScore; */
+  scores[activePlayer] += currentScore;
+  activePlayer === 0
+    ? (score0El.textContent = scores[activePlayer])
+    : (score1El.textContent = scores[activePlayer]);
+  //Check score > 100. If so, finish the game
+  if (Number(`score${activePlayer}El`.textContent) >= 100) {
+    console.log('End game');
+    //Score < 100, switch players
+  } else {
+    switchPlayer();
   }
 });
