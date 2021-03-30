463cc9624397129f5a7b9a140092a90b704ff766
Freddy <atm64ioc@gmail.com>
2021-03-30 12:50:05 +0200

    end dice game

--- complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -14,14 +14,36 @@ const btnHold = document.querySelector('.btn--hold');
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
 
-//Starting conditions
-const scores = [0, 0]; //Total scores of the players
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let playing, currentScore, activePlayer, scores;
 
-let currentScore = 0;
-let activePlayer = 0;
+const init = function () {
+  //Game is active again
+  playing = true;
+  //Reset scores
+  scores = [0, 0]; //Total scores of the players
+  currentScore = 0;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  for (let i = 0; i < scores.length; i++) {
+    scores[i] = 0;
+  }
+  //Reset active player
+  activePlayer = 0;
+  //Hide the dice
+  diceEl.classList.add('hidden');
+  //Remove player winner state
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  //Set player 0 as player active
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+
+//Starting conditions
+init();
+
 const switchPlayer = function () {
   //Set current score back to 0
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -37,38 +59,55 @@ const switchPlayer = function () {
 
 //Button listeners
 btnRoll.addEventListener('click', function () {
-  //1. Generate random number
-  const diceNumber = Math.trunc(Math.random() * 6) + 1;
-  //2. Display dice
-  diceEl.classList.remove('hidden');
-  /*Instead of an if or switch statement, we'll use a template literal
-  to directly assign the name of the image source*/
-  diceEl.src = `dice-${diceNumber}.png`;
-  //3. Check for rolled 1. If true, switch to next player
-  if (diceNumber !== 1) {
-    //Add the score to the current score
-    currentScore += diceNumber;
-    //show currentScore of the active player
-    document.getElementById(
-      `current--${activePlayer}`
-    ).textContent = currentScore;
-  } else {
-    switchPlayer();
+  //If the game is not finished yet
+  if (playing) {
+    //1. Generate random number
+    const diceNumber = Math.trunc(Math.random() * 6) + 1;
+    //2. Display dice
+    diceEl.classList.remove('hidden');
+    /*Instead of an if or switch statement, we'll use a template literal
+    to directly assign the name of the image source*/
+    diceEl.src = `dice-${diceNumber}.png`;
+    //3. Check for rolled 1. If true, switch to next player
+    if (diceNumber !== 1) {
+      //Add the score to the current score
+      currentScore += diceNumber;
+      //show currentScore of the active player
+      document.getElementById(
+        `current--${activePlayer}`
+      ).textContent = currentScore;
+    } else {
+      switchPlayer();
+    }
   }
 });
 
 btnHold.addEventListener('click', function () {
-  //Add current score to active player score
-  /* Number(`score${activePlayer}El`.textContent) += currentScore; */
-  scores[activePlayer] += currentScore;
-  activePlayer === 0
-    ? (score0El.textContent = scores[activePlayer])
-    : (score1El.textContent = scores[activePlayer]);
-  //Check score > 100. If so, finish the game
-  if (Number(`score${activePlayer}El`.textContent) >= 100) {
-    console.log('End game');
-    //Score < 100, switch players
-  } else {
-    switchPlayer();
+  //If the game is not finished yet
+  if (playing) {
+    //Add current score to active player score
+    /* Number(`score${activePlayer}El`.textContent) += currentScore; */
+    scores[activePlayer] += currentScore;
+    activePlayer === 0
+      ? (score0El.textContent = scores[activePlayer])
+      : (score1El.textContent = scores[activePlayer]);
+    //Check score > 100. If so, finish the game
+    if (scores[activePlayer] >= 100) {
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+      //Hide the dice
+      diceEl.classList.add('hidden');
+      //Game is no longer active
+      playing = false;
+      //Score < 100, switch players
+    } else {
+      switchPlayer();
+    }
   }
 });
+
+btnNew.addEventListener('click', init);
