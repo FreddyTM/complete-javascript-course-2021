31149364677fbc38b90d9b7ad8af07af6310de71
Freddy <atm64ioc@gmail.com>
2021-04-03 09:32:26 +0200

    Behind-the-scenes second commit

--- complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -2,8 +2,24 @@
 
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
-  console.log(firstName);
-  return calcAge;
+
+  function printAge() {
+    let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    console.log(millenial);
+    /* console.log(add(2, 3)); ERROR, NOT DEFINED (ONLY IN STRICT MODE)*/
+  }
+
+  printAge();
+  return age;
 }
 
 const firstName = 'Jonas';
