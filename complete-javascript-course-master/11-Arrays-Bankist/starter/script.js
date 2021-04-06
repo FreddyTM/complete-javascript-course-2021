ebcd34f46ee1cf7666f8feda4551969c4fffdde8
Freddy <atm64ioc@gmail.com>
2021-04-06 12:18:29 +0200

    Arrays 1st commit

--- complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -74,3 +74,46 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+/////////////////////////////////////////////////
+//SIMPLE ARRAY METHODS
+let arr = ['a', 'b', 'c', 'd', 'e'];
+// SLICE (Returns a sliced array (start_position, end_position)) (IT DOESN'T CHANGE)
+console.log(arr.slice(2)); //["c", "d", "e"] without end, we get from position 2 to the end
+console.log(arr.slice(2, 4)); //["c", "d"] with an end, we get from position 2 to position end - 1
+console.log(arr.slice(-2)); //["d", "e"] negative position, start from the end
+console.log(arr.slice(-1)); //["e"]
+console.log(arr.slice(1, -2)); //["b", "c"]
+console.log(arr.slice()); //["a", "b", "c", "d", "e"] --SHALLOW COPY
+console.log([...arr]); //["a", "b", "c", "d", "e"] --SHALLOW COPY
+
+// SPLICE - adds, inserts or delete elements of he original array (IT DOES CHANGE)
+//console.log(arr.splice(2)); // arr = ["c", "d", "e"]
+arr.splice(-1); //arr = ["a", "b", "c", "d"]
+console.log(arr);
+//second parameter is delete_count, so deletion starts at start_position, and deletes delete_count elements
+arr.splice(1, 2);
+console.log(arr); //arr = ["a", "d"]
+
+const months = ['Jan', 'March', 'April', 'June'];
+months.splice(1, 0, 'Feb'); //if delete_count is 0, 'Feb' is inserted at position 1
+// inserts at index 1
+console.log(months);
+// expected output: Array ["Jan", "Feb", "March", "April", "June"]
+months.splice(4, 1, 'May'); //if delete_count is 1, element at position 4 is replaced by 'May'
+// replaces 1 element at index 4
+console.log(months);
+// expected output: Array ["Jan", "Feb", "March", "April", "May"]
+
+// REVERSE - reverse the elements of the original array (IT DOES CHANGE)
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse()); // ["f", "g", "h", "i", "j"]
+console.log(arr2); // ["f", "g", "h", "i", "j"]
+
+// CONCAT - the original arrays are not changed
+const letters = arr.concat(arr2);
+console.log(letters); //["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+console.log([...arr, ...arr2]); //the same as concat()
+
+// JOIN - Gets a string with all the elements of an array separated by the character passed as argument
+console.log(letters.join(' - ')); //a - b - c - d - e - f - g - h - i - j
