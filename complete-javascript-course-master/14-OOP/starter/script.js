e4d24aa9dabf8d0faf79d43a3855b6cfdba87351
Freddy <atm64ioc@gmail.com>
2021-04-13 19:34:24 +0200

    OOP (208)

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -69,3 +69,28 @@ Person {firstName: "Jonas", birthYear: 1991}
     species: "Homo Sapiens" -> part of the prototype 
     constructor: ƒ (firstName, birthYear) -> part of the prototype
     __proto__: Object */
+
+///////////////////////////////////////
+// Prototypal Inheritance on Built-In Objects
+console.log(jonas.__proto__);
+// Object.prototype (top of prototype chain)
+console.log(jonas.__proto__.__proto__);
+console.log(jonas.__proto__.__proto__.__proto__);
+
+console.dir(Person.prototype.constructor);
+
+const arr = [3, 6, 6, 5, 6, 9, 9]; // new Array === []
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+
+console.log(arr.__proto__.__proto__);
+
+//Just an example. DON'T add funtcionts to the prototype of built-in objects like Array
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+console.dir(x => x + 1);
